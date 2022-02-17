1573e603accab5df318cd32113a7d58025e40f88
77ABDYLDAEV <abbasabdyldaev07@gmai.com>
2022-02-17 20:39:05 +0600

    gfhgfgfg

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int num;
+    num = 10;
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
